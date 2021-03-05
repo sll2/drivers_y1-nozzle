144d479b716e82ca20914363bb0e9bbbf3013793
Michael Anderson <manders2@illinois.edu>
2021-03-05 10:00:44 -0800

    new ramping initialization

--- ramp_startup/runLassenBatch.sh
+++ ramp_startup/runLassenBatch.sh
@@ -13,7 +13,7 @@ conda deactivate
 conda activate mirgeDriver.Y1nozzle
 export PYOPENCL_CTX="port:tesla"
 #export PYOPENCL_CTX="0:2"
-jsrun_cmd="jsrun -g 1 -a 1 -n 4"
+jsrun_cmd="jsrun -g 1 -a 1 -n 1"
 export XDG_CACHE_HOME="/tmp/$USER/xdg-scratch"
 $jsrun_cmd js_task_info
 $jsrun_cmd python -u -m mpi4py ./nozzle.py
