e6117fb55205e6ba2962a933d50183fa61c5439a
anderson2981 <41346365+anderson2981@users.noreply.github.com>
2021-07-06 15:02:54 -0500

    Anderson linting (#8)
    * updates
    
    * new branch to clean up linting issues
    
    * one more
    
    * add ci
    
    * now CI?
    
    * CI on nozzle.py only
    
    * formatting
    
    * flake8-quotes
    
    * syntax updates
    
    * syntax
    
    * python version?
    
    * pylint ?
    
    * moar
    
    * PYLINTgit add .pylintrc.yml ! and githooks pre-commit
    
    * ugh
    
    * try building emirge and then running pylint
    
    * now buildMirge can operate with ssh or https support
    
    * Update nozzle.py
    
    Co-authored-by: Matt Smith <mjsmith6@illinois.edu>
    
    * Update nozzle.py
    
    Co-authored-by: Matt Smith <mjsmith6@illinois.edu>
    
    * Update nozzle.py
    
    Co-authored-by: Matt Smith <mjsmith6@illinois.edu>
    
    * Update nozzle.py
    
    Co-authored-by: Matt Smith <mjsmith6@illinois.edu>
    
    * Update nozzle.py
    
    Co-authored-by: Matt Smith <mjsmith6@illinois.edu>
    
    * Update nozzle.py
    
    Co-authored-by: Matt Smith <mjsmith6@illinois.edu>
    
    * updates
    
    * few more syntax
    
    * try again
    
    * merge to update
    
    * add smoke test to ci
    
    * ci updates
    
    Co-authored-by: Matt Smith <mjsmith6@illinois.edu>

--- buildMirge.sh
+++ buildMirge.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+usage()
+{
+  echo "Usage: $0 [options]"
+  echo "  --use-ssh         Use ssh-keys to clone emirge/mirgecom"
+  echo "  --help            Print this help text."
+}
+
+opt_git_ssh=0
+
+while [[ $# -gt 0 ]];do
+  arg=$1
+  shift
+  case $arg in
+    --use-ssh)
+      opt_git_ssh=1
+      ;;
+    --help)
+      usage
+      exit 0
+      ;;
+    *)
+      echo "=== Error: unknown argument '$arg' ."
+      usage
+      exit 1
+      ;;
+  esac
+done
 
 # default install script for mirgecom, will build with the current development head
 echo "Building MIRGE-Com from the current development head"
@@ -7,18 +34,29 @@ echo "***WARNING*** may not be compatible with this driver ***WARNING"
 echo "consider build scripts from <platforms> as appropriate"
 
 if [ -z "$(ls -A emirge)" ]; then
-  git clone git@github.com:illinois-ceesd/emirge.git emirge
+  if [ $opt_git_ssh -eq 1 ]; then
+    echo "git clone git@github.com:illinois-ceesd/emirge.git emirge"
+    git clone git@github.com:illinois-ceesd/emirge.git emirge
+  else
+    echo "git clone https://github.com/illinois-ceesd/emirge.git emirge"
+    git clone https://github.com/illinois-ceesd/emirge.git emirge
+  fi
 else
   echo "emirge install already present. Remove to build anew"
 fi
 
 cd emirge
+git_method=""
+if [ $opt_git_ssh -eq 1 ]; then
+  git_method="--git-ssh"
+fi
 
 if [ -z ${CONDA_PATH+x} ]; then
   echo "CONDA_PATH unset, installing new conda with emirge"
-  ./install.sh --env-name=mirgeDriver.Y1nozzle --git-ssh --branch=y1-production
+  echo "./install.sh --env-name=mirgeDriver.Y1nozzle $git_method --branch=y1-production"
+  ./install.sh --env-name=mirgeDriver.Y1nozzle $git_method --branch=y1-production
 else
   echo "Using existing Conda installation, ${CONDA_PATH}"
-  ./install.sh --conda-prefix=$CONDA_PATH --env-name=mirgeDriver.Y1nozzle --git-ssh --branch=y1-production
+  echo "./install.sh --conda-prefix=$CONDA_PATH --env-name=mirgeDriver.Y1nozzle $git_method --branch=y1-production"
+  ./install.sh --conda-prefix=$CONDA_PATH --env-name=mirgeDriver.Y1nozzle $git_method --branch=y1-production
 fi
-
